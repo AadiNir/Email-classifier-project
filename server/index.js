b3b72351ff1022ddccb08cbe856e7702fadc8301
Aadinir <aadiniranjan27@gmail.com>
2024-06-09 13:58:41 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const router = require('../server/api/index');
 const cors = require("cors");
-const allowedOrigins = ['https://email-classifier-project-bz4a1qynr-aadinirs-projects.vercel.app']; 
+const allowedOrigins = ['https://email-classifier-project-f6kel6gqo-aadinirs-projects.vercel.app']; 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
